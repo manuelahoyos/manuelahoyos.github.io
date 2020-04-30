b8abf322ac579ca5423c07759b1b909237332f84
Manuela Hoyos <mhoyos80@gmail.com>
2020-04-29 22:19:11 -0400

    commit

--- Leaflet/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoibWFudWVsYWhveW9zIiwiYSI6ImNrNjg2ZWdpcjAxcnQza3FmZ2t5cnFwNGwifQ.7P6nusW885CGj1c-Jk55MA";
--- World-Pandemics/web/static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-var API_KEY = "pk.eyJ1IjoiZGszMzE0NSIsImEiOiJjazZjdjl2Z2MwOTR4M2RxYmEwNnl6MWhnIn0.k3AYKSALlnOno85VXYplAw";
